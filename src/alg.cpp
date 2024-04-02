afba501b8c8042845052de9cff6c997371ba677f
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-04-02 22:58:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,31 +26,12 @@ int countPairs2(int *arr, int len, int value) {
   return sum;
 }
 int countPairs3(int *arr, int len, int value) {
-  int result = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int l_bound = i;
-    int r_bound = len;
-    while (l_bound < r_bound - 1) {
-      int middle = (r_bound + l_bound) / 2;
-      if (arr[i] + arr[middle] == value) {
-        result++;
-        int j = middle + 1;
-        while ((arr[i] + arr[j] == value) && (j < r_bound)) {
-          result++;
-          j++;
-        }
-        j = middle - 1;
-        while ((arr[i] + arr[j] == value) && (j > l_bound)) {
-          result++;
-          j--;
-        }
-        break;
-      } else if (arr[i] + arr[middle] > value) {
-        r_bound = middle;
-      } else {
-        l_bound = middle;
-      }
+    int count = 0;
+    int i = 0;
+    while (i < len) {
+        int number = value - arr[i];
+        count = count + cbinsearch(&arr[i + 1], len - i - 1, number);
+        i++;
     }
-  }
-  return result;
+    return count;
 }
